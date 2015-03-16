d74f61972bf9aa4ea618f1af1401c657a637914d
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2015-03-17 01:15:39 +0900

    add program options

--- src/Main.hs
+++ src/Main.hs
@@ -2,29 +2,78 @@
 
 module Main (main) where
 
+import           Control.Monad
 import           Data.Maybe
+import           Data.Monoid
 import           Data.String
+import           Options.Applicative
 import           System.Console.Terminfo.Color       as Terminfo
 import           System.Console.Terminfo.PrettyPrint
-import           System.Environment
 import           Text.Highlighting.Kate              as Kate
-import qualified Text.Highlighting.Kate.Styles       as Styles
-import           Text.PrettyPrint.Free
+import           Text.PrettyPrint.Free               (hcat, vcat)
 
 main :: IO ()
-main = do
-  [file] <- getArgs
+main = join $ execParser opts where
+  opts = info (helper <*> cmd)
+         ( fullDesc
+        <> progDesc "Print the content of FILE with syntax highlighting"
+        <> header "cless: Colorized LESS" )
+
+  cmd = process
+        <$> switch ( long "list-langs"
+                  <> short 'L'
+                  <> help "Show the list of supported languages" )
+        <*> switch ( long "list-styles"
+                  <> short 'S'
+                  <> help "Show the list of supported styles" )
+        <*> optional (strOption ( long "lang"
+                  <> short 'l'
+                  <> metavar "LANG"
+                  <> help "Specify language name" ) )
+        <*> optional (strOption ( long "style"
+                  <> short 's'
+                  <> metavar "STYLE"
+                  <> help "Specify style name (default 'pygments')" ) )
+        <*> argument str (metavar "FILE")
+
+styles :: [(String, Style)]
+styles =
+  [ ("pygments"  , pygments  )
+  , ("kate"      , kate      )
+  , ("espresso"  , espresso  )
+  , ("tango"     , tango     )
+  , ("haddock"   , haddock   )
+  , ("monochrome", monochrome)
+  , ("zenburn"   , zenburn   )
+  ]
+
+process :: Bool -> Bool -> Maybe String -> Maybe String -> FilePath -> IO ()
+process True _ _ _ _ =
+  mapM_ putStrLn languages
+
+process _ True _ _ _ =
+  mapM_ (putStrLn . fst) styles
+
+process _ _ mb_lang mb_stylename file = do
   con <- readFile file
 
-  let [lang] = languagesByFilename file
+  let lang = fromMaybe (error "cannot determin language")
+             $ mb_lang <|> listToMaybe (languagesByFilename file)
+
       ss = highlightAs lang con
 
-  display $ ppr Styles.tango ss
+      style = maybe pygments findStyle mb_stylename
+
+      findStyle name =
+        fromMaybe (error $ "invalid style name: " ++ name)
+        $ lookup name styles
+
+  displayLn $ ppr style ss
 
 ppr :: Style -> [SourceLine] -> TermDoc
 ppr Style{..} = vcat . map (hcat . map token) where
-  token (tokenType, str) =
-    tokenEffect tokenType $ fromString str
+  token (tokenType, ss) =
+    tokenEffect tokenType $ fromString ss
 
   tokenEffect :: TokenType -> TermDoc -> TermDoc
   tokenEffect tokenType =
